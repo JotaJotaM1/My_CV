80880cd2f871932772bdb0d490a967cd7ac2247c
JotaJotaM1 <johnjames_89@hotmail.com>
2024-01-30 20:02:27 -0500

    update general, add aboutme and row in prohjects

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 const nav = document.querySelector("#nav");
-const open = document.querySelector("#open"); 
+const open = document.querySelector("#open");
 const close = document.querySelector("#close");
 
-open.addEventListener("click" , () =>{
-    nav.classList.add("visible");
-})
+open.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
 
-close.addEventListener("click", ()=>{
-    nav.classList.remove("visible");
-})
+close.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
+
+//
+
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver(handleIntersection, options);
+
+const targets = document.querySelectorAll(
+  ".textH2Skill, .textH2LengSpeak, .textAgi, .textAbout, .titleh1"
+);
+
+targets.forEach((target) => {
+  observer.observe(target);
+});
